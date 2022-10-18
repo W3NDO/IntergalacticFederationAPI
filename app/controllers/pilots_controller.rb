f36758c1e119064f6e554d958889e6913d4bf290
W3NDO <wendonyang@gmail.com>
2022-10-18 22:37:56 +0300

    chore(main): fixing spelling errors

--- app/controllers/pilots_controller.rb
+++ app/controllers/pilots_controller.rb
@@ -45,7 +45,7 @@ class PilotsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pilot
-      @pilot = pilot.find(params[:id])
+      @pilot = Pilot.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
